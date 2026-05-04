c8d3628f58af6ea9c56f5a9bc7944addf6473b38
Ale <riccardi.alessandro04@gmail.com>
2026-05-04 11:58:00 +0200

    Implement function to send game data to server
    Added a new function 'inviaReferto' to send game data to the server via a POST request.

--- client/js/api.js
+++ client/js/api.js
@@ -5,3 +5,21 @@ const api = {
         return true;
     }
 };
+
+async function inviaReferto(datiPartita) {
+    const jsonData = JSON.stringify(datiPartita); // Serializzazione JSON
+    
+    try {
+        const response = await fetch('http://localhost:3000/api/salva_partita', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: jsonData
+        });
+        
+        if (response.ok) {
+            console.log("Partita salvata con successo!");
+        }
+    } catch (error) {
+        console.error("Errore di comunicazione col server", error);
+    }
+}
